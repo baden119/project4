4def1a61eec3456c72a71b0629969f83428b41e1
baden allen <baden.j.allen@gmail.com>
2020-09-23 18:04:04 +1000

    Some javascript work. i dont know. my brain hurts. javascript is the worst.

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,18 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
+// console.log("DO SOMETING!!!!!!!!!!!");
+  document.querySelector('#edit_post').onclick = function() {
 
-console.log("DO SOMEEEETHING!!!")
+    const edit_form = document.createElement("FORM");
 
-document.querySelector('#edit_post').onclick = function() {
-  console.log("EDIT POST CLICKED!!!")
+    const edit_box = document.createElement("INPUT");
+    edit_box.type='TEXTAREA';
+    edit_box.value = document.querySelector(`#post_body_${this.name}`).innerHTML;
+    edit_box.id = "edit_box"
 
-  document.querySelector('#post_body').style.color = 'red';
+    const submit = document.createElement("INPUT");
+    submit.innerHTML = "Save Changes"
+    submit.className = "btn btn-primary btn-sm"
+    submit.type = "SUBMIT"
 
-  // const body = document.querySelector('#post_body').value;
-  //
-  // console.log(body)
-};
+    edit_form.appendChild(edit_box);
+    edit_form.appendChild(submit);
 
+    document.querySelector(`#post_body_${this.name}`).style.display = 'none';
+    document.querySelector(`#edit_box_${this.name}`).append(edit_form);
+    // document.querySelector(`#edit_box_${this.name}`).append(submit);
 
+    // document.querySelector('#submit').onclick = function() {
+    //   console.log("submit clicked!");
+    //
+    //   console.log(document.querySelector('#edit_box').innerHTML);
 
+    // };
+
+  };
 
 });
